c9a8dcbcd7404f8d61498bd4606a2ed1a7df796f
kungminlin <33560439+kungminlin@users.noreply.github.com>
2018-02-18 18:14:40 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>generationOn LAHS</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta property="og:title" content="generationOn LAHS">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://generationon.lahs.club">
+	<!-- <meta property="og:image" content="./assets/images/main-image.jpg"> -->
+	<meta property="og:description" content="there's some description.">
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+</head>
+<body>
+	<nav>
+		<div class="nav">
+			<span id="logo">generationOn LAHS</span>
+			<ul>
+				<li>
+					<a href="#">Home</a></li><li>
+				<a href="#">About</a></li><li>
+				<a href="#">Projects</a></li><li>
+				<a href="#">Contact Us</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="title">
+		<span id="t_logo"></span>
+		<h2>Service since 2017</h2>
+	</div>
+	<div class="section light">
+		<div class="content">
+			<h1>Community Service</h1>
+			<br />
+			<p>Lorem Episolon I don't speak French</p>
+		</div>
+		<div class="image"><img src="assets/img/img.png"></div>
+	</div>
+
+	<div class="section darken">
+		<div class="content-right">
+			<p>tests</p>
+		</div>
+	</div>
+
+	<!-- <div class="section center">
+		<span class="btn"><a href="#">Contact Us!</a></span>
+	</div> -->
+
+	<footer>
+		<p>&copy; 2018 <a href="https://generationon.lahs.club">LAHS generationOn Club</a> | Website created by Kung-Min Lin in association with <a href="https://lahs.club">LAHS Hack Club</a></p>
+	</footer>
+
+	<script src="assets/js/index.js"></script>
+
+</body>
+</html>
